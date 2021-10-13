3a1b0eeb564d2c6444e1f80e19ab9ab3ee5de494
KARIMANSHAWKI <kariman0800@gmail.com>
2021-10-13 23:04:23 +0200

    Rest API List CRUD Operations

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const movieRoute = require("./routes/movies");
-
+const listRoute = require("./routes/lists");
 
 const app = express();
 dotenv.config();
 app.use(express.json());
 
 //routes
-app.use('/api/auth', authRoute);
-app.use('/api/users', userRoute);
-app.use('/api/movies', movieRoute);
-
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
 
 mongoose
   .connect(process.env.MONGO_URL, {
@@ -23,7 +23,6 @@ mongoose
   })
   .then(() => console.log("Database connected!"))
   .catch((err) => console.log(err));
-
 
 app.listen(8000, () => {
   console.log("Backend server is running!");
