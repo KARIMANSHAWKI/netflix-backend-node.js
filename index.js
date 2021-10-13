a9e8f220335b99be4453e3a85617d71ac7093e6d
KARIMANSHAWKI <kariman0800@gmail.com>
2021-10-13 20:47:31 +0200

    Rest API Movie CRUD Operations

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
 
 
 const app = express();
@@ -12,8 +13,7 @@ app.use(express.json());
 //routes
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
-
-
+app.use('/api/movies', movieRoute);
 
 
 mongoose
